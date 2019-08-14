3424c533ca558e4426ebe1a29056bbcdaf82ebbc
Oleksandr Ivashchenko <sanuch@gmail.com>
2019-08-14 08:10:43 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+// import List from './modules/list/List';
+
+const Fragment = React.Fragment;
+
+const App: React.FC = () => {
+  return (
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path="/" component={React}/>
+          </Switch>
+        </Fragment>
+      </Router>
+  );
+};
+
+export default App;
